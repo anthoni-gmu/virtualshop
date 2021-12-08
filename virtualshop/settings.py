dbc6b8971f7cadcf81bc42ae5cc723dd8fb7bf8b
anthoni-gmu <softwaretoni21@gmail.com>
2021-12-08 13:20:01 -0500

    category_views

--- virtualshop/settings.py
+++ virtualshop/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
+    'apps.store',
     'apps.core'
 
 ]
@@ -65,6 +66,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'apps.store.context_processors.menu_categories',
+                
             ],
         },
     },
